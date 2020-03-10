2469071d5f9fff3b984e5517982fffca93a6ac40
hackerrank <me@hackerrank.com>
2020-03-10 12:43:18 +0100

    create virtual environment and djago framework

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\juanm\OneDrive\Documentos\python_scripts\Proyectos\lead_manager_react_django\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
